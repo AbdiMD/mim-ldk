badbf23b952f37ce7879fb86e4c782426f5536dd
AbdiMD <50539805+AbdiMD@users.noreply.github.com>
2022-09-22 00:49:06 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+// function Tambahsiswa (nama, alamat, usia, gender, kelas) {
+//     this.nama = nama;
+//     this.alamat = alamat;
+//     this.usia = usia;
+//     this.gender = gender;
+//     this.kelas = kelas;
+// }
+
+function Tambahsiswa (nama, alamat, usia, gender, kelas) {
+    this.nama = nama;
+    this.alamat = alamat;
+    this.usia = usia;
+    this.gender = gender;
+    this.kelas = kelas;
+}
+
+
+let siswa1 = new Tambahsiswa('Gerard', 'Jl. Hasanuddin no. 13', 24, 'Laki-Laki', 'Kelas 1')
+
+
+
+
+
+
+
+
